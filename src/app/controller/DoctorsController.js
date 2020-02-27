3d1fd80abe801a9db6241abbc6d50164090a3f49
rogerio bispo <rogerio_pd@yahoo.com.br>
2020-02-27 08:30:08 -0300

    .

--- src/app/controller/DoctorsController.js
+++ src/app/controller/DoctorsController.js
@@ -1,16 +1,24 @@
 import User from '../models/User';
 import File from '../models/File';
+import Especialty from '../models/Specialty';
 
 class DoctorsController {
     async index(_, res) {
         const doctor = await User.findAll({
             where: { doctor: true },
-            attributes: ['id', 'name', 'email'],
-            include: {
-                model: File,
-                as: 'avatar',
-                attributes: ['name', 'path', 'url'],
-            },
+            attributes: ['id', 'name', 'crm'],
+            include: [
+                {
+                    model: File,
+                    as: 'avatar',
+                    attributes: ['name', 'path', 'url'],
+                },
+                {
+                    model: Especialty,
+                    as: 'specialty',
+                    attributes: ['name'],
+                },
+            ],
         });
         res.json(doctor);
     }
