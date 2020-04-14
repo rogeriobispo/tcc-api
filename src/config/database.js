1eedc53cdf9defadff23886933edc47c7df13937
rogerio bispo <rogerio_pd@yahoo.com.br>
2020-04-14 17:37:31 -0300

    .

--- src/config/database.js
+++ src/config/database.js
@@ -13,3 +13,18 @@ module.exports = {
         underscoredAll: true,
     },
 };
+
+// module.exports = {
+//     dialect: 'postgres',
+//     timezone: '+05:30',
+//     host: 'localhost',
+//     port: '5431',
+//     username: 'root',
+//     password: 'root',
+//     database: 'clinicalCare',
+//     define: {
+//         timesstamps: true,
+//         underscored: true,
+//         underscoredAll: true,
+//     },
+// };
