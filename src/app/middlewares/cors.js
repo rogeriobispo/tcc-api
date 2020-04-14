60f013b804887b9a5efede3b924b489fd306c1e9
rogerio bispo <rogerio_pd@yahoo.com.br>
2020-04-14 19:01:00 -0300

    .

--- src/app/middlewares/cors.js
+++ src/app/middlewares/cors.js
@@ -1,8 +1,10 @@
 export default async (_, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header(
         'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept'
+        'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'
     );
     next();
 };
